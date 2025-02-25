ed5698671303ee5a4b4626417b0a1eaea4631dd7
Yusa21 <157543456+Yusa21@users.noreply.github.com>
2025-02-25 11:07:34 +0100

    Controladores y modelos de clinica y veterinario

---
+++ scripts/ClinicaController.py
@@ -0,0 +1,38 @@
+from DAO import Database
+import sqlite3
+from scripts.model.Clinica import Clinica
+
+class ClinicaController:
+    def __init__(self):
+        self.db = Database("veterinaria_clinic.db")
+        
+    def getClinicas(self):
+        self.db.cursor.execute("SELECT * FROM Clinicas")
+        return self.db.cursor.fetchall()
+    
+    def getClinica(self, id: int):
+        self.db.cursor.execute("SELECT * FROM Clinicas WHERE id = ?", (id,))
+        return self.db.cursor.fetchone()
+    
+    def postClinica(self, clinica: Clinica):
+        try:
+            self.db.cursor.execute(
+                "INSERT INTO Clinicas (Municipio, Provincia, name) VALUES (?, ?, ?)", 
+                (clinica.municipio, clinica.provincia, clinica.name)
+            )
+            self.db.conn.commit()
+            return self.db.cursor.lastrowid
+        except sqlite3.IntegrityError:
+            print("Error: Constraint violation")
+            return -1
+   
+    def putClinica(self, id: int, clinica: Clinica):
+        self.db.cursor.execute(
+            "UPDATE Clinicas SET Municipio = ?, Provincia = ?, name = ? WHERE id = ?", 
+            (clinica.municipio, clinica.provincia, clinica.name, id)
+        )
+        self.db.conn.commit()
+        
+    def deleteClinica(self, id: int):
+        self.db.cursor.execute("DELETE FROM Clinicas WHERE id = ?", (id,))
+        self.db.conn.commit()
