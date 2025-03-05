14e2953900bd3f3ce2ccda41f3bc0a43e82d41fe
Yusa21 <157543456+Yusa21@users.noreply.github.com>
2025-03-05 17:57:17 +0100

    Ui de los logins y register

--- scripts/DAO.py
+++ scripts/DAO.py
@@ -4,12 +4,8 @@ import sqlite3
 
 class Database:
     def __init__(self):
-        # Get the absolute path to the project directory
         base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         db_path = os.path.join(base_dir, "database", "veterinary_clinic.db")
-
-        # Make sure the directory exists
-        os.makedirs(os.path.dirname(db_path), exist_ok=True)
 
         self.conn = sqlite3.connect(db_path)
         self.cursor = self.conn.cursor()
