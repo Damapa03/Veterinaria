a9d4ba95d90cda80d050129bc789aec09f2d5c15
Daniel Corral <dani05corral@gmail.com>
2025-03-07 18:57:15 +0100

    commit

---
+++ ui/cita_ui.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'cita.ui'
+##
+## Created by: Qt User Interface Compiler version 6.8.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QGridLayout, QHBoxLayout, QLabel,
+    QMainWindow, QPushButton, QScrollArea, QSizePolicy,
+    QSpacerItem, QSpinBox, QStatusBar, QVBoxLayout,
+    QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(800, 600)
+        MainWindow.setMinimumSize(QSize(800, 600))
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.pushButton = QPushButton(self.centralwidget)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setStyleSheet(u"QPushButton {\n"
+"    background-color: lightgray;\n"
+"    color: black;\n"
+"    border: 2px solid gray;\n"
+"    border-radius: 10px;\n"
+"    padding: 5px;\n"
+"}\n"
+"\n"
+"QPushButton:hover {\n"
+"    background-color: silver;\n"
+"}\n"
+"\n"
+"QPushButton:pressed {\n"
+"    background-color: darkgray;\n"
+"}")
+
+        self.horizontalLayout.addWidget(self.pushButton)
+
+        self.labelColumnas = QLabel(self.centralwidget)
+        self.labelColumnas.setObjectName(u"labelColumnas")
+
+        self.horizontalLayout.addWidget(self.labelColumnas)
+
+        self.spinColumnas = QSpinBox(self.centralwidget)
+        self.spinColumnas.setObjectName(u"spinColumnas")
+        self.spinColumnas.setMinimum(1)
+        self.spinColumnas.setMaximum(5)
+        self.spinColumnas.setValue(3)
+
+        self.horizontalLayout.addWidget(self.spinColumnas)
+
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.horizontalLayout.addItem(self.horizontalSpacer)
+
+        self.btnNuevaCita = QPushButton(self.centralwidget)
+        self.btnNuevaCita.setObjectName(u"btnNuevaCita")
+        self.btnNuevaCita.setStyleSheet(u"QPushButton {\n"
+"    background-color: lightgray;\n"
+"    color: black;\n"
+"    border: 2px solid gray;\n"
+"    border-radius: 10px;\n"
+"    padding: 5px;\n"
+"}\n"
+"\n"
+"QPushButton:hover {\n"
+"    background-color: silver;\n"
+"}\n"
+"\n"
+"QPushButton:pressed {\n"
+"    background-color: darkgray;\n"
+"}")
+        icon = QIcon(QIcon.fromTheme(u"list-add"))
+        self.btnNuevaCita.setIcon(icon)
+
+        self.horizontalLayout.addWidget(self.btnNuevaCita)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
+        self.scrollArea = QScrollArea(self.centralwidget)
+        self.scrollArea.setObjectName(u"scrollArea")
+        self.scrollArea.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollAreaWidgetContents = QWidget()
+        self.scrollAreaWidgetContents.setObjectName(u"scrollAreaWidgetContents")
+        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 780, 521))
+        self.gridLayout = QGridLayout(self.scrollAreaWidgetContents)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+
+        self.verticalLayout.addWidget(self.scrollArea)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Gesti\u00f3n de Citas M\u00e9dicas", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.labelColumnas.setText(QCoreApplication.translate("MainWindow", u"Columnas:", None))
+        self.btnNuevaCita.setText(QCoreApplication.translate("MainWindow", u"A\u00f1adir Cita", None))
+    # retranslateUi
+
